2b8753e83d48964a049ef75fce89879b4b290f84
jingsam <jing-sam@qq.com>
2017-02-11 16:21:26 +0800

    add iview

--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,9 @@
+import 'iview/dist/styles/iview.css'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import iview from 'iview'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+Vue.use(iview){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
